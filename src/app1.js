17812e86671b6c8f0b97bea96d38ea8a572157d2
Karthik Mohan <karthikmohan314@gmail.com>
2025-08-05 15:21:37 +0200

    git working?

--- src/app1.js
+++ src/app1.js
@@ -1,16 +1,16 @@
 var fs= require('fs')
 
-// fs.readFile('calc.js','utf8',function(err,data){
-//     console.log(data);
-// })
+fs.readFile('calc.js','utf8',function(err,data){
+    console.log(data);
+})
 
-// fs.writeFile('calc1.js','console.log("done")',function(err,data){
-//     console.log("file created");
-// })
+fs.writeFile('calc1.js','console.log("done")',function(err,data){
+    console.log("file created");
+})
 
-// fs.appendFile('calc1.js','console.log("done");',function(err,data){
-//     console.log("file created");
-// })
+fs.appendFile('calc1.js','console.log("done");',function(err,data){
+    console.log("file created");
+})
 
 fs.unlink('calc1.js',function(err){
     console.log("file deleted commit 6");      // calc1.js deleted from folder 
